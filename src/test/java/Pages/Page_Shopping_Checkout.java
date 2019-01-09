4ea9ad4354113d52c8d375505758f515a8b83d53
leviettien1808 <leviettien1808@gmail.com>
2019-01-09 14:56:53 +0700

    Get XPath of Shopping - Checkout part.

---
+++ src/test/java/Pages/Page_Shopping_Checkout.java
@@ -0,0 +1,7 @@
+package Pages;
+
+public class Page_Shopping_Checkout {
+    public static String btn_BasketPopup_Order = ".//button[@id='order_product']";
+    // Đang suy nghĩ để tìm cách làm.
+    //.//div[@id='view_cart']//child::tr[2]//td[2]
+}
