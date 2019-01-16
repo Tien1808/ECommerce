4650918f5cb7d529138479e6889442140e2fd63a
leviettien1808 <leviettien1808@gmail.com>
2019-01-16 08:19:20 +0700

    Assign 'static' into setUp() and downCrash(). Add comment.

--- src/test/java/Commons/SetUp.java
+++ src/test/java/Commons/SetUp.java
@@ -7,16 +7,22 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class SetUp {
-    public static WebDriver driver = null;
-    public static String url = "http://103.95.199.151/mobilevn/";
-    public static String username = "123@gmail.com";
-    public static String password = "abcdef";
-    public static String keyword = "htc";
-    public static Users user = null;
-    public static Product product= null;
+    private static WebDriver driver = null;
+    private static String url = "http://103.95.199.151/mobilevn/";
+    private static String username = "123@gmail.com";
+    private static String password = "abcdef";
+    private static String keyword = "htc";
+    private static Users user = null;
+    private static Product product= null;
+
+    // Em có ý định sẽ dời những biến có liên quan ở phía trên
+    // vô các hàm ở phía dưới. Ví dụ: setUp() thành setUp(Webdriver...)
+    // Em sẽ làm sau, sau khi em nắm rõ phần Config.properties trong mục
+    // Resources!!! Mục đích em làm vậy là vì như vậy nó đúng chuẩn như
+    // thầy nói và để xóa mấy cái vạch vàng ở bên cạnh!!!
 
     @BeforeMethod
-    public void setUp() {
+    public static void setUp() {
         System.setProperty("webdriver.chrome.driver", ".\\src\\test\\Resources\\drivers\\chromedriver.exe");
         driver.get(url);
         driver.manage().window().maximize();
@@ -29,7 +35,7 @@ public class SetUp {
     }
 
     @AfterMethod
-    public void downCrash() {
+    public static void downCrash() {
         driver.quit();
     }
 
