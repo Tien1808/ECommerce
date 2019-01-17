e839a7b3d29be794b55fdcb0cdce35b3c2cc5b4c
thaitai <phamthaitai2303@gmail.com>
2019-01-17 22:38:43 +0700

    update

--- src/test/java/Actions/Shopping_ManageBasket_Action.java
+++ src/test/java/Actions/Shopping_ManageBasket_Action.java
@@ -5,9 +5,13 @@ import Pages.Shopping_ManageBasket_Page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Shopping_ManageBasket_Action {
@@ -18,19 +22,20 @@ public class Shopping_ManageBasket_Action {
     }
 
     public static void clickBuyAnotherButton(WebDriver driver){
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-        driver.findElement(By.xpath(Shopping_ManageBasket_Page.btn_BasketPopup_BuyAnother)).click();
+        //driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        //driver.findElement(By.xpath(Shopping_ManageBasket_Page.btn_BasketPopup_BuyAnother)).click();
+        WebDriverWait wait=new WebDriverWait(driver, 20);
+        WebElement element= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Shopping_ManageBasket_Page.btn_BasketPopup_BuyAnother)));
+        element.click();
+
     }
 
     public static void CheckBasketPopupDisplay(WebDriver driver) {
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         WebElement BasketPopupTitle = driver.findElement(By.xpath(Shopping_ManageBasket_Page.txt_BasketPopup_Title));
         boolean Check = BasketPopupTitle.isEnabled();
-        // Anh, chỗ ni mình có thể chuyển từ 'Check == true'
-        // thành 'Check'. Vì em thấy cái vạch màu vàng ở bên
-        // cạnh gợi ý như vậy. Vì đơn giản ý em là sửa lại
-        // cho nó hết màu vàng bên cạnh nhìn cho nó đẹp ý mà!!!
-        if (Check == true) {
+
+        if (Check) {
             System.out.printf("TC_Shopping_ManageBasket_01: Pass. Basket pop up is displayed");
         } else {
             System.out.printf("TC_Shopping_ManageBasket_01: Failed. Basket pop up isn't displayed");
@@ -51,10 +56,14 @@ public class Shopping_ManageBasket_Action {
 
     // Nút này hiển thị ở phía dưới của mỗi cardview.
     public static void clickAddToBasketButton(WebDriver driver, String Productname) {
-
+/**
+        driver.manage().timeouts().implicitlyWait(1,TimeUnit.SECONDS);
         WebElement ProductAddCartBtn = driver.findElement(By.xpath(".//div[@class='products-content-label' and contains(.,'"+Productname+"')]/button[@class='btn btn-danger cart_class']"));
         ProductAddCartBtn.click();
-
+*/
+        WebDriverWait wait=new WebDriverWait(driver, 20);
+        WebElement element= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//div[@class='products-content-label' and contains(.,'"+Productname+"')]/button[@class='btn btn-danger cart_class']")));
+        element.click();
     }
 
     /**
@@ -95,51 +104,57 @@ public class Shopping_ManageBasket_Action {
         }
     }
 
-    public static void CheckTotalPrice(WebDriver driver) throws ParseException {
+    public static <limit> void CheckTotalPrice(WebDriver driver) throws ParseException {
         driver.manage().timeouts().implicitlyWait(1,TimeUnit.SECONDS);
 
-        for (int i = 1; i>0; i++){
+            List ProductList = driver.findElements(By.xpath("//div[@id=\"view_cart\"]/table/tbody/tr/td/img"));
 
-            WebElement NameProduct = driver.findElement(By.xpath(".//div[@id=\"view_cart\"]/table/tbody/tr["+i+"]/td[2]"));
+            for (int i = 1; i<= ProductList.size(); i++){
 
-            if(NameProduct.isDisplayed()==true){
-                WebElement discount = driver.findElement(By.xpath(".//div[@id='view_cart']/table/tbody/tr["+i+"]/td[3]/label"));
-                WebElement unitPrice = driver.findElement(By.xpath(".//div[@id='view_cart']/table/tbody/tr["+i+"]/td[5]/label"));
-                WebElement estimatedSubtotal = driver.findElement(By.xpath(".//div[@id='view_cart']/table/tbody/tr["+i+"]/td[6]/label"));
+                if(driver.findElement(By.xpath(".//div[@id=\"view_cart\"]/table/tbody/tr[" + i + "]/td[2]")).isEnabled()){
+                    NumberFormat format2 = NumberFormat.getNumberInstance();
+                    Number Limit = format2.parse(String.valueOf(ProductList.size()));
+                    int limit = Integer.parseInt(String.valueOf(Limit));
+                    WebElement ProductName = driver.findElement(By.xpath(".//div[@id=\"view_cart\"]/table/tbody/tr["+i+"]/td[2]"));
+                    WebElement discount = driver.findElement(By.xpath(".//div[@id='view_cart']/table/tbody/tr["+i+"]/td[3]/label"));
+                    WebElement unitPrice = driver.findElement(By.xpath(".//div[@id='view_cart']/table/tbody/tr["+i+"]/td[5]/label"));
+                    WebElement estimatedSubtotal = driver.findElement(By.xpath(".//div[@id='view_cart']/table/tbody/tr["+i+"]/td[6]/label"));
 
-                String Discoutn = discount.getAttribute("textContent") ;
-                NumberFormat format1 = NumberFormat.getPercentInstance();
-                Number a = format1.parse(Discoutn);
-                String UnitPrice = unitPrice.getAttribute("textContent");
-                NumberFormat format2 = NumberFormat.getNumberInstance();
-                Number b = format2.parse(UnitPrice);
-                String EstimatedSubtotal = estimatedSubtotal.getAttribute("textContent");
-                Number c = format2.parse(EstimatedSubtotal);
+                    String productName = ProductName.getAttribute("textContent");
+                    String Discoutn = discount.getAttribute("textContent") ;
+                    NumberFormat format1 = NumberFormat.getPercentInstance();
+                    Number a = format1.parse(Discoutn);
+                    String UnitPrice = unitPrice.getAttribute("textContent");
+                    Number b = format2.parse(UnitPrice);
+                    String EstimatedSubtotal = estimatedSubtotal.getAttribute("textContent");
+                    Number c = format2.parse(EstimatedSubtotal);
 
-                double A = Double.parseDouble(String.valueOf(a));
+                    double A = Double.parseDouble(String.valueOf(a));
 
-                int B = Integer.parseInt(String.valueOf(b));
-                int C = Integer.parseInt(String.valueOf(c));
+                    int B = Integer.parseInt(String.valueOf(b));
+                    int C = Integer.parseInt(String.valueOf(c));
 
-                double Check = B-(A*(B*1.0));
-                int SubPriceWithDiscount = (int) Check;
-                if (A > 0){
-                    if (C == SubPriceWithDiscount){
-                        System.out.println("TC_Shopping_ManageBasket_06: Passed Total Price = "+SubPriceWithDiscount+"đ");
-                    }else {
-                        System.out.printf("TC_Shopping_ManageBasket_06 (Discount): Failed");
+                    double Check = B-(A*(B*1.0));
+                    int SubPriceWithDiscount = (int) Check;
+                    if (A > 0 ){
+                        if (C == SubPriceWithDiscount && i < limit){
+                            System.out.println("Product name: "+productName+". Total Price = "+SubPriceWithDiscount+"đ");
+                        } else if (C == SubPriceWithDiscount && i == limit) {
+                            System.out.println("Product name: "+productName+". Total Price = "+SubPriceWithDiscount+"đ");
+                            System.out.println("TC_Shopping_ManageBasket_06: Passed");
+                        }else {
+                            System.out.println("TC_Shopping_ManageBasket_06: Failed");
+                        }
+                    }else if (B == C && i < limit){
+                        System.out.println("Product name: "+productName+". Total Price = "+C+"đ");
+                    }else if (B == C && i == limit){
+                        System.out.println("Product name: "+productName+". Total Price = "+C+"đ");
+                        System.out.println("TC_Shopping_ManageBasket_06: Passed");
+                    } else {
+                        System.out.println("TC_Shopping_ManageBasket_06: Failed");
                     }
-                }else if (B == C){
-                    System.out.println("TC_Shopping_ManageBasket_06: Passed Total Price = "+C+"đ");
-                }else {
-                    System.out.printf("TC_Shopping_ManageBasket_06: Failed");
                 }
-            }else {
-                System.out.println("Basket Empty");
-                break;
             }
-        }
-
     }
 
     public static void clickLoginLink(WebDriver driver) {
